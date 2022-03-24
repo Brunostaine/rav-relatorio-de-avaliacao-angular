8ce587b546c340d26d9782a0c0c5d71da041c71a
Bruno Staine <brunostaine@hotmail.com>
2022-03-23 22:18:56 -0300

    :soon: Add cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    
   ],
   imports: [
     BrowserModule,
