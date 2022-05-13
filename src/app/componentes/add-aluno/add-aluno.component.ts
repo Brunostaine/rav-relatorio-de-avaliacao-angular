0fcd2e76cb53de4e04a40bd902c62b78e619f84b
Bruno Staine <brunostaine@hotmail.com>
2022-05-13 11:20:45 -0300

    formatação

--- src/app/componentes/add-aluno/add-aluno.component.ts
+++ src/app/componentes/add-aluno/add-aluno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AddAlunoService } from './service/add-aluno.service';
@@ -10,23 +10,21 @@ import { AddAlunoService } from './service/add-aluno.service';
   styleUrls: ['./add-aluno.component.css'],
 })
 export class AddAlunoComponent implements OnInit {
-
   listarPerguntas: any = [];
 
   turmas: any = [];
   // Formulario reativo
   // formulario!: FormGroup;
-formulario!: FormGroup
+  formulario!: FormGroup;
   // mostrar mensagem de carregamento
   isLoading = false;
   // Radio button dinamico
   radioButtonOpcoes: any = [];
-  
+
   constructor(
     private addAlunoService: AddAlunoService,
     private router: Router,
-    private fb: FormBuilder,
-    
+    private fb: FormBuilder
   ) {}
 
   // buildOption(){
@@ -34,24 +32,19 @@ formulario!: FormGroup
   // }
 
   ngOnInit(): void {
-
-
     // Formulario
     this.formulario = this.fb.group({
       nome: [null, [Validators.required, Validators.minLength(3)]],
       turma: [null, [Validators.required]],
       perguntas: [null, [Validators.required]],
-      
-    })
+    });
 
-    console.log(this.formulario)
+    console.log(this.formulario);
 
     // Radio button opções
 
-
-
     this.radioButtonOpcoes = this.addAlunoService.radioButton();
-    console.log(this.radioButtonOpcoes)
+    console.log(this.radioButtonOpcoes);
 
     // Lista as perguntas
     this.isLoading = true;
@@ -69,8 +62,6 @@ formulario!: FormGroup
       },
     });
 
-    
-
     // Lista as turmas
     this.addAlunoService.listarTurmas().subscribe({
       next: (resp) => {
@@ -80,19 +71,17 @@ formulario!: FormGroup
     });
   }
 
- 
-
   onSubmit() {
-    console.log(this.formulario.value)
+    console.log(this.formulario.value);
 
-    this.addAlunoService.SubmitFormulario(this.formulario.value).subscribe()
-    this.router.navigate([''])
+    this.addAlunoService.SubmitFormulario(this.formulario.value).subscribe();
+    this.router.navigate(['']);
 
     this.addAlunoService.emitirRelatorioCriado.subscribe(
-      this.listarPerguntas = console.log(this.formulario)
-    )
+      (this.listarPerguntas = console.log(this.formulario))
+    );
   }
-  
+
   onCancel(): void {
     this.router.navigate(['']);
   }
