c977cd66a0fedf968c1c8da48d585b4bdbafb80f
Bruno Staine <brunostaine@hotmail.com>
2022-05-14 16:49:32 -0300

    refatoramento

--- src/app/componentes/add-aluno/add-aluno.module.ts
+++ src/app/componentes/add-aluno/add-aluno.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppMaterialModule } from 'src/app/shared/app-material/app-material.module';
+
 
 import { SharedModule } from '../shared/shared.module';
 import { AddAlunoComponent } from './add-aluno.component';
@@ -15,7 +15,7 @@ import { AddAlunoComponent } from './add-aluno.component';
   ],
   imports: [
     CommonModule,
-    AppMaterialModule,
+    
     ReactiveFormsModule,
     HttpClientModule,
     SharedModule
